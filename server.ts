c5c504a49b1cedefdf48df151b3b51ad9a3510bf
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-03-18 15:24:33 +0900

    csp

--- server.ts
+++ server.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import { apiRouter } from "./routes/api";
+import crypto from "crypto";
 
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/api", apiRouter);
 
 app.get("/", (req, res, next) => {
   res.send("Top page");
+});
+
+app.get("/csp", (req, res) => {
+  const nonceValue = crypto.randomBytes(16).toString("base64");
+  res.header(
+    "Content-Security-Policy",
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+      "object-src 'none';" +
+      "base-uri 'none';" +
+      "require-trusted-types-for 'script';"
+  );
+  res.render("csp", { nonce: nonceValue, val: "好きな値を返す" });
 });
 
 app.listen(PORT, () => {
